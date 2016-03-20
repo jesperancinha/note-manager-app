f72e5fc27937c20b72418cfa69d73f4ec8e2eb45
Joao Esperancinha <jofisaes@hotmail.com>
2016-03-20 11:38:35 +0100

    SZ-AMS-002 Initial model for DB

---
+++ title-text-adder-api/src/test/resources/com/steelzack/titletextadder/create_script.sql
@@ -0,0 +1,12 @@
+create user 'tta'@'localhost' identified by 'tta';
+
+create database tta_db;
+
+create table titles(
+   title_id INT NOT NULL AUTO_INCREMENT,
+   title VARCHAR(100) NOT NULL,
+   title_text VARCHAR(500) NOT NULL,
+   title_submission_date DATE,
+   title_text_score TINYINT,
+   PRIMARY KEY ( title_id )
+);
--- title-text-adder-app/README.md
+++ title-text-adder-app/README.md
@@ -6,3 +6,8 @@
 * mvn tomcat7:deploy
 * mvn tomcat7:undeploy
 * mvn tomcat7:redeploy
+
+## References:
+
+* http://www.solrtutorial.com/solr-in-5-minutes.html
+* http://blog.comperiosearch.com/blog/2014/08/28/indexing-database-using-solr/
