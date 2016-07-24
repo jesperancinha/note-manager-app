1982a81fcd7bc5470ccbd7333a4ab33061e9ef6f
Joao <jofisaes@hotmail.com>
2016-07-25 00:07:51 +0200

    SZ-N Adds queue project using Spring Boot

---
+++ note-manager-queue/src/main/java/com/steelzack/nodemanager/NoteManagerQueueApplication.java
@@ -0,0 +1,12 @@
+package com.steelzack.nodemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagerQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteManagerQueueApplication.class, args);
+	}
+}
