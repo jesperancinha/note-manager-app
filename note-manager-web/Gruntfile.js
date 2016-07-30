d8124c0bad42f8288d9dfaf2538d175f9a8867b7
Joao <jofisaes@hotmail.com>
2016-07-30 17:24:36 +0200

    SZ-N Grunt task incorported with MVN with NPM

--- note-manager-web/Gruntfile.js
+++ note-manager-web/Gruntfile.js
@@ -1,16 +1,21 @@
 module.exports = function(grunt){
-    pkg: grunt.file.readJSON('package.json'),
 
     grunt.loadNpmTasks('grunt-package-modules');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.initConfig({
+
       packageModules: {
         dist: {
           src: 'package.json',
           dest: 'src/main/webapp'
         },
       },
+
+      clean:['src/main/webapp/package.json']
+      ,
+
     });
 
-    grunt.registerTask('default', ['packageModules']);
+    grunt.registerTask('default', ['packageModules', 'clean']);
 }
