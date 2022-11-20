0df3ac1e1bf2802a2fe318fd0a6b4ae98cdb4acd
Joao Esperancinha <jofisaes@gmail.com>
2022-11-20 16:09:10 +0100

    Typo correction

--- note-manager-app/src/test/kotlin/org/jesperancinha/manager/note/ApplicationTest.kt
+++ note-manager-app/src/test/kotlin/org/jesperancinha/manager/note/ApplicationTest.kt
@@ -1,32 +1,12 @@
 package org.jesperancinha.manager.note
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.auth.*
-import io.ktor.util.*
-import io.ktor.server.auth.jwt.*
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.server.plugins.partialcontent.*
-import io.ktor.server.plugins.*
-import io.micrometer.prometheus.*
-import io.ktor.server.metrics.micrometer.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.websocket.*
-import io.ktor.websocket.*
-import java.time.Duration
-import io.ktor.server.engine.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import manager.note.jesperancinha.org.plugins.*
 import org.jesperancinha.manager.note.plugins.configureRouting
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
