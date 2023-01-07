7e086ce69ef3777ca16204145ea1fc70ac6238ba
Joao Esperancinha <jofisaes@gmail.com>
2023-01-07 12:46:06 +0100

    Another type-safe route

--- nm-simple-ktor/Readme.md
+++ nm-simple-ktor/Readme.md
@@ -1,6 +1,15 @@
 # Note Manager App - Simple Ktor
 
+## Introduction
+
+This is a test app to see how Ktor works and how we can create endpoints and routes in different ways especially in a `type-safe` way.
+
+Current endpoints:
+
+- [http://localhost:8080](http://localhost:8080)
+- [http://localhost:8080/news](http://localhost:8080/news)
+- [http://localhost:8080/messages](http://localhost:8080/messages)
+
 ## References
 
 -   [Type-safe routing](https://ktor.io/docs/type-safe-routing.html)
-
--- nm-simple-ktor/src/main/kotlin/org/jesperancinha/plugins/Routing.kt
+++ nm-simple-ktor/src/main/kotlin/org/jesperancinha/plugins/Routing.kt
@@ -8,12 +8,19 @@ import io.ktor.server.routing.*
 
 
 val allArticles = listOf(
-    "Lars: If you write that in the book I will throw the book away",
-    "Merchant: I would never call him Mr. Comp you know. You don't even know what the whole word is or what it means. Imagine that it's something intimate like a past someone special to him?"
+    "Lars: If you write that in the book I will throw the book away.",
+    "Merchant: I would never call him Mr. Comp you know. You don't even know what the whole word is or what it means. Imagine that it's something intimate like a past someone special to him ?"
+)
+
+val allMessages = listOf(
+    "Priest: I understand where you are coming from, but as much as you may want to label this as a harmless joke, it is not. Words have power especially nicknames. What is positive for many may not be felt the same way for the receiver of this nonsense. So please don't do this anymore."
 )
 
 @Resource("/news")
-class NewsReport()
+class NewsReport
+
+@Resource("/messages")
+class Messages
 
 fun Application.configureRouting() {
     routing {
@@ -22,6 +29,10 @@ fun Application.configureRouting() {
         }
         get<NewsReport> {
             call.respondText(allArticles.random())
+        }
+
+        get<Messages> {
+            call.respondText(allMessages.random())
         }
     }
 }
