9f4dd947e6ede74598d1b1717f4a51aa75bf2692
Joao Esperancinha <jofisaes@gmail.com>
2023-01-07 10:22:26 +0100

    Migrates to JDK19

--- nm-simple-ktor/src/main/kotlin/org/jesperancinha/plugins/Routing.kt
+++ nm-simple-ktor/src/main/kotlin/org/jesperancinha/plugins/Routing.kt
@@ -1,13 +1,10 @@
 package org.jesperancinha.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-
     routing {
         get("/") {
             call.respondText("Hello World!")
