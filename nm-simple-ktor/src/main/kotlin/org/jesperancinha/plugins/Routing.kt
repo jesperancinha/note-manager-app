74d9807499f48a49ed0112a31319ad34b83e5943
Joao Esperancinha <jofisaes@gmail.com>
2023-01-07 13:35:04 +0100

    More type-safe routing

--- nm-simple-ktor/src/main/kotlin/org/jesperancinha/plugins/Routing.kt
+++ nm-simple-ktor/src/main/kotlin/org/jesperancinha/plugins/Routing.kt
@@ -1,10 +1,12 @@
 package org.jesperancinha.plugins
 
+import io.ktor.http.*
 import io.ktor.resources.*
 import io.ktor.server.application.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.time.LocalDateTime
 
 
 val allArticles = listOf(
@@ -13,26 +15,34 @@ val allArticles = listOf(
 )
 
 val allMessages = listOf(
-    "Priest: I understand where you are coming from, but as much as you may want to label this as a harmless joke, it is not. Words have power especially nicknames. What is positive for many may not be felt the same way for the receiver of this nonsense. So please don't do this anymore."
+    "Priest: I understand where you are coming from, but as much as you may want to label this as a harmless joke, it is not. Words have power especially nicknames. What is positive for many may not be felt the same way for the receiver of this nonsense. So please don't do this anymore.",
+    "Mrs Rose: I know Mr. Pereira from a long time ago my dear. He can probably help you. I know his stories and it all adds up. He can definitely help you"
 )
 
 @Resource("/news")
 class NewsReport
 
 @Resource("/messages")
-class Messages
+class Messages {
+    @Resource("{id}")
+    class Id(val parent: Messages = Messages(), val id: Int)
+}
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+
         get<NewsReport> {
             call.respondText(allArticles.random())
         }
 
         get<Messages> {
             call.respondText(allMessages.random())
+        }
+        get<Messages.Id> { message ->
+            call.respondText("Message received:\n${allMessages[message.id]}\non the ${LocalDateTime.now()}", status = HttpStatusCode.OK)
         }
     }
 }
