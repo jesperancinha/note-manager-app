c1cbf9aab94e317c1da426da543b0c2ac4ff1f73
João Esperancinha <jofisaes@gmail.com>
2024-11-17 16:51:21 +0100

    updates code to ktor 3

--- note-manager-app/src/main/kotlin/org/jesperancinha/manager/note/plugins/Sockets.kt
+++ note-manager-app/src/main/kotlin/org/jesperancinha/manager/note/plugins/Sockets.kt
@@ -1,18 +1,15 @@
 package org.jesperancinha.manager.note.plugins
 
+import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
-import java.time.Duration
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.server.routing.*
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureSockets() {
     install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
-        timeout = Duration.ofSeconds(15)
+        pingPeriod = 15.seconds
+        timeout = 15.seconds
         maxFrameSize = Long.MAX_VALUE
         masking = false
     }
